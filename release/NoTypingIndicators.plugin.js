9a45b72c947488ba9c6a90e058528d8c98e0a1ff
Nick Mykkanen <th3nickm2@gmail.com>
2022-09-28 13:16:29 -0500

    create basic plugin

--- release/NoTypingIndicators.plugin.js
+++ release/NoTypingIndicators.plugin.js
@@ -1,9 +1,11 @@
 /**
- * @name Example Plugin
- * @description Stop telling other people that you're typing!
- * @version 0.0.1
+ * @name No Typing Indicators
+ * @description Extraordinarily basic plugin to stop telling other people that you're typing!
+ * @version 1.0.0
  * @author TheNickm2
  * @authorId 441377078634610688
+ * @website https://github.com/TheNickm2/no-typing-indicators-bd
+ * @source https://github.com/TheNickm2/no-typing-indicators-bd/blob/main/release/NoTypingIndicators.plugin.js
  */
 /*@cc_on
 @if (@_jscript)
@@ -28,7 +30,7 @@
     WScript.Quit();
 
 @else@*/
-const config = {"info":{"name":"Example Plugin","authors":[{"name":"TheNickm2","discord_id":"441377078634610688","github_username":"TheNickm2","twitter_username":"TheNickm2"}],"version":"0.0.1","description":"Stop telling other people that you're typing!","github":"","github_raw":""},"changelog":[{"title":"A New Plugin Appeared!","items":["Initial plugin"]}],"main":"index.js"};
+const config = {"info":{"name":"No Typing Indicators","authors":[{"name":"TheNickm2","discord_id":"441377078634610688","github_username":"TheNickm2","twitter_username":"TheNickm2"}],"version":"1.0.0","description":"Extraordinarily basic plugin to stop telling other people that you're typing!","github":"https://github.com/TheNickm2/no-typing-indicators-bd","github_raw":"https://github.com/TheNickm2/no-typing-indicators-bd/blob/main/release/NoTypingIndicators.plugin.js"},"changelog":[{"title":"A Wild Plugin Appeared!","items":["Initial plugin creation."]}],"main":"index.js"};
 class Dummy {
     constructor() {this._config = config;}
     start() {}
@@ -50,75 +52,20 @@ if (!global.ZeresPluginLibrary) {
  
 module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
      const plugin = (Plugin, Library) => {
+  const { Logger, Patcher, WebpackModules } = Library;
 
-    const {Logger, Patcher, Settings} = Library;
+  return class NoTypingIndicators extends Plugin {
+    onStart() {
+      Logger.log("Started");
+      const TypingModule = WebpackModules.getByProps("startTyping");
+      Patcher.instead(TypingModule, "startTyping", () => {});
+    }
 
-    return class ExamplePlugin extends Plugin {
-        constructor() {
-            super();
-            this.defaultSettings = {};
-            this.defaultSettings.color = "#ff0000";
-            this.defaultSettings.option = 50;
-            this.defaultSettings.keybind = [162, 74];
-            this.defaultSettings.radio = "weiner";
-            this.defaultSettings.slider1 = 30;
-            this.defaultSettings.slider2 = 54;
-            this.defaultSettings.textbox = "nothing";
-            this.defaultSettings.switch1 = false;
-            this.defaultSettings.switch2 = true;
-            this.defaultSettings.switch3 = true;
-            this.defaultSettings.switch4 = false;
-            this.defaultSettings.file = undefined;
-        }
-
-        onStart() {
-            Logger.log("Started");
-            Patcher.before(Logger, "log", (t, a) => {
-                a[0] = "Patched Message: " + a[0];
-            });
-        }
-
-        onStop() {
-            Logger.log("Stopped");
-            Patcher.unpatchAll();
-        }
-
-        getSettingsPanel() {
-            return Settings.SettingPanel.build(this.saveSettings.bind(this), 
-                new Settings.SettingGroup("Example Plugin Settings").append(
-                    new Settings.Textbox("Textbox", "This should be a description of what this setting is about or blank", this.settings.textbox, (e) => {this.settings.textbox = e;}),
-                    new Settings.Dropdown("Select", "This should be a description of what this setting is about or blank", this.settings.option, [
-                        {label: "Test 1", value: "weiner"},
-                        {label: "Test 2", value: 50},
-                        {label: "Test 3", value: JSON.stringify({label: "Test 1", value: "weiner"})},
-                    ], (e) => {this.settings.option = e;}),
-                    new Settings.SettingGroup("Example Plugin SubSettings", {shown: true}).append(
-                        new Settings.RadioGroup("Generic RadioGroup", "This should be a description of what this setting is about or blank", this.settings.radio, [
-                            {name: "Test 1", value: "weiner", desc: "This is the first test", color: "#ff0000"},
-                            {name: "Test 2", value: 50, desc: "This is the second test", color: "#00ff00"},
-                            {name: "Test 3", value: JSON.stringify({label: "Test 1", value: "weiner"}), desc: "This is the third test", color: "#0000ff"},
-                        ], (e) => {this.settings.radio = e;}),
-                        new Settings.Switch("Switch1", "This should be a description of what this setting is about or blank", this.settings.switch1, (e) => {this.settings.switch1 = e;}),
-                        new Settings.Switch("Switch2", "This should be a description of what this setting is about or blank", this.settings.switch2, (e) => {this.settings.switch2 = e;}),
-                        new Settings.Switch("Switch3", "This should be a description of what this setting is about or blank", this.settings.switch3, (e) => {this.settings.switch3 = e;}),
-                        new Settings.Switch("Switch4", "This should be a description of what this setting is about or blank", this.settings.switch4, (e) => {this.settings.switch4 = e;})
-                    )
-                ),
-
-                new Settings.SettingGroup("Example Advanced Settings").append(
-                    new Settings.ColorPicker("Color Example", "This should be a description of what this setting is about or blank", this.settings.color, (e) => {this.settings.color = e;}),
-                    new Settings.Keybind("Keybind", "This should be a description of what this setting is about or blank", this.settings.keybind, (e) => {this.settings.keybind = e;}),
-                    new Settings.Slider("Slider", "This should be a description of what this setting is about or blank", 0, 100, this.settings.slider1, (e) => {this.settings.slider1 = e;}),
-                    new Settings.Slider("Slider2", "This should be a description of what this setting is about or blank", 0, 90, this.settings.slider2, (e) => {this.settings.slider2 = e;}, {
-                        markers: [0, 9, 18, 27, 36, 45, 54, 63, 72, 81, 90],
-                        stickToMarkers: true
-                    }),
-                    new Settings.FilePicker("FilePicker", "This should be a description of what this setting is about or blank", (e) => {if (e) this.settings.file = e.path;})
-                )
-            );
-        }
-    };
-
+    onStop() {
+      Logger.log("Stopped");
+      Patcher.unpatchAll();
+    }
+  };
 };
      return plugin(Plugin, Api);
 })(global.ZeresPluginLibrary.buildPlugin(config));
